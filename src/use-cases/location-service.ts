8241fee7b7658fa26b388976f8685b4651708507
Eunovo <eunovo9@gmail.com>
2019-12-12 00:53:10 +0100

    add location service

---
+++ src/use-cases/location-service.ts
@@ -0,0 +1,24 @@
+import Location from "../data/location";
+import LocationRepository from "../repos/location-repository";
+
+class LocationService {
+    private locationRepo: LocationRepository;
+
+    constructor(locationRepo: LocationRepository) {
+        this.locationRepo = locationRepo;
+    }
+
+    async logLocation(location: Location) {
+        let savedLocation = await this.locationRepo.saveLocation(location);
+        return savedLocation;
+    }
+
+    async findAllLocationsByUserFromTo(
+        userId: Object, startTime: Date, endTime?: Date) {
+            let locations = await this.findAllLocationsByUserFromTo(
+                userId, startTime, endTime);
+            return locations;
+    }
+}
+
+export default LocationService;
