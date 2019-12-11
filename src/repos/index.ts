c2d23c1d36a7f9b82efff17944323e78263ec83f
Eunovo <eunovo9@gmail.com>
2019-12-12 00:43:29 +0100

    expose location repo

--- src/repos/index.ts
+++ src/repos/index.ts
@@ -1,5 +1,8 @@
 import { getDb } from '../config/db';
+import LocationRepository from './location-repository';
+import LocationRepoImpl from './location-repo-impl';
 import UserRepository from './user-repository';
 import UserRepoImpl from './user-repo-impl';
 
 export const getUserRepo = (): UserRepository => new UserRepoImpl(getDb());
+export const getLocationRepo = (): LocationRepository => new LocationRepoImpl(getDb());
